8918f85da374128f77564e5f3df4eea21b69b7f6
Dmitry Ryzhov <softwaretester6666@gmail.com>
2026-01-13 10:48:27 +0300

    Changed Scanner class 1

---
+++ src/main/java/Scanner/PrintingFirstWord.java
@@ -0,0 +1,13 @@
+package Scanner;
+
+import java.util.Scanner;
+
+class PrintingFirstWord {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String myText = myScanner.next();
+
+        System.out.println(myText);
+
+    }
+}
