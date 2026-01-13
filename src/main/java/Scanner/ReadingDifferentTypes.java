35e465d0ee651aa7cfe6f440fa20d2f9818430d2
Dmitry Ryzhov <softwaretester6666@gmail.com>
2026-01-13 16:33:28 +0300

    updated branch

--- src/main/java/Scanner/ReadingDifferentTypes.java
+++ src/main/java/Scanner/ReadingDifferentTypes.java
@@ -2,23 +2,23 @@ package Scanner;
 
 import java.util.Scanner;
 
-class Survey {
+class ReadingDifferentTypes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Input your age:");
         int age = scan.nextInt();
-        scan.nextLine();
+        scan.nextLine();                                                    //удаление символа \n из буфера
         System.out.println("Input your name:");
         String name = scan.nextLine();
         System.out.println("Input your weight:");
         double weight = scan.nextDouble();
-        scan.nextLine();
+        scan.nextLine();                                                    //удаление символа \n из буфера
         System.out.println("Input your favorite movie:");
         String movie = scan.nextLine();
         System.out.println("Do you like Java? (true/false)");
         boolean userLikeJava = scan.nextBoolean();
-        scan.nextLine();
+        scan.nextLine();                                                    //удаление символа \n из буфера
         System.out.println("Input the reason:");
         String reason = scan.nextLine();
 
