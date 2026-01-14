840c486a7c8988f35ca1ad90262aaf718adb96cb
Dmitry Ryzhov <softwaretester6666@gmail.com>
2026-01-14 10:22:37 +0300

    added new class console

---
+++ src/main/java/Scanner/Console.java
@@ -0,0 +1,39 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public final class Console {
+    private static final Scanner SC = new Scanner(System.in);
+
+    // "прячем" конструктор чтобы никто не создал экземпляр
+    private Console() {}
+
+    public static String nextLine() {
+        // предоставляем только метод nextLine (никакие другие недоступны)
+        return SC.nextLine();
+    }
+
+    public static int nextInt() {
+        // предоставляем только метод nextInt (никакие другие недоступны)
+        int res = SC.nextInt();
+        // "съедаем" остаточный символ \n в буфере
+        SC.nextLine();
+        return res;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // Console console = new Console();
+        // не можем создать объект, т.к. конструктор скрыт
+
+        System.out.println("Введите ID сотрудника:");
+        int id = Console.nextInt();
+        System.out.println("Введите имя:");
+        String name = Console.nextLine();
+
+        System.out.println("Имя (" + name + ") и ID ("
+                + id + ") сотрудника успешно внесены в БД");
+
+    }
+}
